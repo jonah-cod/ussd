a3d0cb006f47d6c0109602391a3bea2180401f74
jonathan <idolearndaily@gmail.com>
2021-05-26 21:43:09 +0300

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const UssdMenu = require('ussd-menu-builder');
+let menu = new UssdMenu();
 
 const app= express();
 
@@ -11,52 +12,89 @@ app.get('/', (req, res)=>{
     res.send("success");
 })
 
-app.post('/', (req, res)=>{
 
-    // console.log(req.body);
-    const {phoneNumber, text, sessionId} = req.body;
+let result = [];
+ 
+// Define menu states
+menu.startState({
+    run: () => {
+        
+        menu.con('Welcome. Please provide your 1st name, last name and age:' +
+            '\n Reply with 1 to continue' +
+            '\n Reply with 2 to cancel'
+            );
+    },
+    
+    next: {
+        '1': 'first name',
+        '2': 'cancel'
+        
+    }
+});
+ 
+menu.state('cancel', {
+    run: ()=>{
+        menu.end('USSD cancelled!')
+    }
+})
 
-    let responce;
-     let result;
-    if(text === ''){
-        responce = 'CON enter your first name';
+menu.state('first name', {
+    run: () => {
+        
+        menu.con('Enter your first name')
+        
+    },
+
+    
+        defaultNext:'last name'
+        
+    
+});
+ 
+menu.state('last name', {
+    run: () => {
+        menu.con('Enter last name:');
+        result.push(menu.val);
+    },
+    
+    defaultNext: 'age'
+});
+ 
+
+menu.state('age', {
+    run: () => {
+        
+        menu.con('enter your age');
+        result.push(menu.val);
+        
+    },
+
+    defaultNext: 'feedback'
+});
+
+menu.state('feedback', {
+    run: () => {
+        
+      let message;
+        result.push(menu.val);
+        if (parseInt(result[2])>55) {
+            message = 'Please retire'
+        } else {
+            message = 'Your name is ' + result[0] + ' ' + result[1] + ' ' + result[2] + ' years continue'
+        }
+        menu.end(message);
         
     }
 
-    if(text!==''){
-        result = text.split('*');
-        console.log(result);
-        if (result.length===1) {
-            responce = 'CON enter your last name'
-            
-          
-        }else if(parseInt(result[0])!== ''){
-            console.log(result.length);
-            if(result.length>1){
-            responce = 'CON enter your age'
-                console.log(result.length);
-                if (parseInt(result[1])!== ''){
-                    let age = result[2]
-                    if (result.length === 3) {
-                        if (parseInt(age)>55) {
-                        responce = 'END please retire'
-                    }
-                    else{
-                        console.log(age);
-                        responce = 'END Your name is ' + result[0]+' ' + result[1]+ ' ' + age +'. years continue working'
-                    }
-                    }
-                    
-                }
-            }
-        }
-    
-     }
-    setTimeout(()=>{
-        res.send(responce)
-        res.end
-    }, 2000);
-})
+});
+ 
+
+ //linking with express
+app.post('/', function(req, res){
+    menu.run(req.body, ussdResult => {
+        res.send(ussdResult);
+    });
+});
 
 
 app.listen(PORT, ()=>{
